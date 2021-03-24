c2075abe9cf9edb04f15b3bb53acf82053700862
Chris Curran <chriscurran8@yahoo.com>
2021-03-24 07:49:11 +0000

    fixing bug with bag view

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
+from products.models import Product
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -20,5 +23,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)

f57c5a87f72d297245292e3c8ed86a2295353b40
Chris Curran <chriscurran8@yahoo.com>
2021-04-30 18:56:01 +0000

    removed secret key and debug

--- freelance_graphix_designer/settings.py
+++ freelance_graphix_designer/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '09ilh+h%#vc=kj3-55go5utipy*f6z!a(+!@dw5g_=uo8o(luu'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['freelance-graphix-designer.herokuapp.com', 'localhost']
 

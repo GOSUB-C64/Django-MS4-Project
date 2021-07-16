c115d194b29f7e9d11cb8866002971390183e7ed
Chris Curran <chriscurran8@yahoo.com>
2021-07-16 20:12:49 +0000

    Added AWS_S3_OBJECT_PARAMETERS

--- freelance_graphix_designer/settings.py
+++ freelance_graphix_designer/settings.py
@@ -178,6 +178,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    #Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     #S3 Bucket config vars
     AWS_STORAGE_BUCKET_NAME = 'cc2021-freelance-graphix-designer'
     AWS_S3_REGION_NAME = 'eu-west-2'
